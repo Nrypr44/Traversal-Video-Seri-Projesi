c903c1d3d0f3929cc3e7d68ebc9b024aa709a232
Nrypr44 <160053539+Nrypr44@users.noreply.github.com>
2025-07-01 12:51:00 +0300

    Add files via upload

---
+++ BusinessLayer/Container/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Abstract.AbstractUow;
+using BusinessLayer.Concrete;
+using BusinessLayer.Concrete.UowConcrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.UnitofWork;
+using DTOLayer.DTOs.AnnouncementDTOs;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Container
+{
+    public static class Extensions
+    {
+        public static void  ContainerDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<ICommentService, CommentManager>();
+            services.AddScoped<ICommentDal, EfCommentDal>();
+            services.AddScoped<IDestinationService, DestinationManager>();
+            services.AddScoped<IDestinationDal, EfDestinationDal>();
+            services.AddScoped<IAppUserService, AppUserManager>();
+            services.AddScoped<IAppUserDal, EfAppUserDal>();
+            services.AddScoped<IReservationService, ReservationManager>();
+            services.AddScoped<IReservationDal, EfReservationDal>();
+            services.AddScoped<IGuideService, GuideManager>();
+            services.AddScoped<IGuideDal, EfGuideDal>();
+            services.AddScoped<IExcelService, ExcelManager>();
+            services.AddScoped<IPdfService, PdfManager>();
+            services.AddScoped<IContactUsDal, EfContactUsDal>();
+            services.AddScoped<IContactUsService, ContactUsManager>();
+            services.AddScoped<IAnnouncementDal, EfAnnouncementDal>();
+            services.AddScoped<IAnnouncementService, AnnouncementManager>();
+            services.AddScoped<IAccountDal, EfAccountDal>();
+            services.AddScoped<IAccountService, AccountManager>();
+            services.AddScoped<IUowDal, UowDal>();
+        }
+
+        public static void CustomerValidator(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<AnnouncementAddDto>,AnnouncementValidator>();
+        }
+    }
+}
